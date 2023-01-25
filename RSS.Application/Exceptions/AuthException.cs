2ad556ee74a5f8ce44a0715f2ee6e9b10ddbe089
bekirov <vbekirovv@gmail.com>
2023-01-26 01:30:01 +0200

    Added required auth models, exceptions.

---
+++ RSS.Application/Exceptions/AuthException.cs
@@ -0,0 +1,14 @@
+﻿namespace RSS.Application.Exceptions;
+
+public class AuthException : Exception
+{
+    public AuthException()
+    {
+        
+    }
+
+    public AuthException(string message) : base(message)
+    {
+        
+    }
+}
