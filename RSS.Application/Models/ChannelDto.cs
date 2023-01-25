f248ee0a2e72b70801cbe23fdecd247a20a34d65
bekirov <vbekirovv@gmail.com>
2023-01-26 00:42:05 +0200

    Added business models.

---
+++ RSS.Application/Models/ChannelDto.cs
@@ -0,0 +1,20 @@
+﻿namespace RSS.Application.Models;
+
+public class ChannelDto
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Link { get; set; } = string.Empty;
+
+    public DateTime? LastBuildDate { get; set; }
+
+    public string? Language { get; set; }
+
+    public string? Copyright { get; set; }
+
+    public ChannelImageDto? ChannelImage { get; set; }
+}
---
+++ RSS.Application/Models/ChannelImageDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RSS.Application.Models;
+
+public class ChannelImageDto
+{
+    public string Title { get; set; } = string.Empty;
+
+    public string Url { get; set; } = string.Empty;
+
+    public string Link { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+}
