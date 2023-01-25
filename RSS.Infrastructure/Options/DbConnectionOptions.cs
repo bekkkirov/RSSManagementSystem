32b74f0fdaa0545de70cda4072aec6347f7c6709
bekirov <vbekirovv@gmail.com>
2023-01-25 22:41:02 +0200

    Added database connection options.

---
+++ RSS.Infrastructure/Options/DbConnectionOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace RSS.Infrastructure.Options;
+
+public class DbConnectionOptions
+{
+    public const string SectionName = "ConnectionStrings";
+
+    public string RssContext { get; set; } = string.Empty;
+
+    public string IdentityContext { get; set; } = string.Empty;
+}
