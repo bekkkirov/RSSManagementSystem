26184f5ba33626092b471b72c41d640ac5973d9a
bekirov <vbekirovv@gmail.com>
2023-01-26 01:05:37 +0200

    Added identity.

---
+++ RSS.Infrastructure/Identity/Entities/UserRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RSS.Infrastructure.Identity.Entities;
+
+public class UserRole : IdentityRole<int>
+{
+    
+}
---
+++ RSS.Infrastructure/Identity/IdentityContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RSS.Infrastructure.Identity.Entities;
+
+namespace RSS.Infrastructure.Identity;
+
+public class IdentityContext : IdentityDbContext<UserIdentity, UserRole, int>
+{
+    public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+    {
+
+    }
+}
