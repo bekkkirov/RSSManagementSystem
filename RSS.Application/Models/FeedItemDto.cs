f8e7d764ce839d06e2ef2c20cb6cca05e05b9bd7
bekirov <vbekirovv@gmail.com>
2023-01-26 13:28:14 +0200

    Added feed item dto.

---
+++ RSS.Application/Models/FeedItemDto.cs
@@ -0,0 +1,18 @@
+﻿namespace RSS.Application.Models;
+
+public class FeedItemDto
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Link { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public string? Author { get; set; }
+
+    public DateTime PublishDate { get; set; }
+
+    public int ChannelId { get; set; }
+}
