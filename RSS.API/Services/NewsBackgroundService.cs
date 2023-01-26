c02923b9ae4ab9673e1aebbd8edc2c28a241038d
bekirov <vbekirovv@gmail.com>
2023-01-26 13:31:05 +0200

    Implemented background service to periodically fetch new from rss feeds.

---
+++ RSS.API/Services/NewsBackgroundService.cs
@@ -0,0 +1,32 @@
+﻿using RSS.Application.Interfaces.Repositories;
+using RSS.Application.Interfaces.Services;
+
+namespace RSS.API.Services;
+
+public class NewsBackgroundService : BackgroundService
+{
+    private readonly TimeSpan _period = TimeSpan.FromHours(3);
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public NewsBackgroundService(IServiceScopeFactory scopeFactory)
+    {
+        _scopeFactory = scopeFactory;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var timer = new PeriodicTimer(_period);
+
+        while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            await using var scope = _scopeFactory.CreateAsyncScope();
+
+            var unitOfWork = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+            var channelService = scope.ServiceProvider.GetRequiredService<IChannelService>();
+
+            var links = await unitOfWork.ChannelRepository.ExtractFeedLinksAsync();
+
+            await channelService.FetchNewsAsync(links);
+        }
+    }
+}
